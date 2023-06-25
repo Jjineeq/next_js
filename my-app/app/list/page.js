9309f7d1e492326f104224b745f85805b57c4e1e
Seongho Jang <jang0212@tukorea.ac.kr>
2023-06-26 00:33:51 +0900

    23.06.26

---
+++ my-app/app/list/page.js
@@ -0,0 +1,19 @@
+
+
+export default function List(){
+    return (
+      <div>
+        
+        <h4 className="title"> 상품목록</h4>
+        <div className="food">
+            <h4>상품1 $30</h4>
+        </div>
+        <div className="food">
+            <h4>상품2 $50</h4>
+        </div>
+      </div>
+  
+    )
+  }
+  
+  
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,14 +1,22 @@
+import Link from 'next/link'
+
 export default function Home(){
   let name ='jang'
   let age = 20
   let link = 'https://www.naver.com'
   return (
     <div>
+
+
       <h3 style = {{color:'red', fontSize : '30px'}}>안녕하세요</h3>
       <h4 className="title"> abc</h4>
       <p className="title-sub"> {name}</p>
+
+      
+
       <a href={link}>링크</a>
     </div>
 
   )
 }
+
