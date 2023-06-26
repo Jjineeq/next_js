78fe5144ce73e191988c28f64ead861be753aeb8
Seongho Jang <jang0212@tukorea.ac.kr>
2023-06-26 21:59:10 +0900

    23.06.26

--- my-app/app/list/page.js
+++ my-app/app/list/page.js
@@ -1,16 +1,33 @@
 
 
 export default function List(){
+    let 상품 = ['Tomatoes' ,'Pasta', 'Coconut']
+    let price = [20, 30, 40]
+
     return (
       <div>
         
         <h4 className="title"> 상품목록</h4>
-        <div className="food">
-            <h4>상품1 $30</h4>
+        {
+          상품.map((a, i)=>{
+            return (
+            <div className="food">
+              <h4>{상품[i]} ${price[i]}</h4>
+            </div>
+            )
+          })
+        }
+
+        {/* <div className="food">
+            <h4>{상품[0]} $30</h4>
         </div>
         <div className="food">
-            <h4>상품2 $50</h4>
+            <h4>{상품[1]} $50</h4>
         </div>
+        <div className="food">
+          <h4>{상품[2]} $70</h4>
+
+        </div> */}
       </div>
   
     )
--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,20 +1,15 @@
 import Link from 'next/link'
 
 export default function Home(){
-  let name ='jang'
+  let name ='Jang'
   let age = 20
   let link = 'https://www.naver.com'
   return (
     <div>
 
+      <h4 className="title"> Next js study page</h4>
+      <p className="title-sub"> made by {name}</p>
 
-      <h3 style = {{color:'red', fontSize : '30px'}}>안녕하세요</h3>
-      <h4 className="title"> abc</h4>
-      <p className="title-sub"> {name}</p>
-
-      
-
-      <a href={link}>링크</a>
     </div>
 
   )
